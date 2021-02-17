49548e557a2382f7f5e355a2323d3940225bcdc3
ReineMoberg <reine.moberg3@gmail.com>
2021-02-17 18:46:34 +0100

    Implemented add() in CityDaoIml

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -19,8 +19,12 @@ public class App {
         //List<City> foundByCode = dao.findByCode("SWE");
         //foundByCode.forEach(System.out::println);
 
-        List<City> foundByName = dao.findByName("Los Angeles");
-        foundByName.forEach(System.out::println);
+        //List<City> foundByName = dao.findByName("Los Angeles");
+        //foundByName.forEach(System.out::println);
+
+        City testCity = new City("Test City", "SWE","Test District",500);
+        City testCity2 = dao.add(testCity);
+        System.out.println(testCity2);
 
         //List<City> findAll= dao.findAll();
         //findAll.forEach(System.out::println);
